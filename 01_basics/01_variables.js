45edbf66f5e6ca868210a12af8c3a071a676b09d
samikshabansal7 <samikshabansal990@gmail.com>
2026-05-20 09:05:05 +0000

    let var char

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,11 @@
+const accountid= 144552
+let accountemail="samiksha123@gmail.com"
+var accountpassword="12345"
+let accountstate
+accountcity="jaipur"
+//accountid=23
+accountemail="sam12@gmail.com"
+accountpassword="123"
+accountcity="bengaluru"
+console.log("accountid");
+console.table([accountid,accountemail,accountpassword,accountcity,accountstate])
